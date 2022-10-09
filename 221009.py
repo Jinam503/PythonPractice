8520e477418e3353b97c5bcb36ce1bc24c1b2d83
Jinam503 <jinam1234567@gmail.com>
2022-10-09 13:59:41 +0900

    d

---
+++ 221009.py
@@ -0,0 +1,18 @@
+n = [1,2, 7, 3,4,5]
+nn = [6,7,8,9]
+print(n+nn)
+print(sorted(n))
+print(n)
+
+n.sort()
+print(n)
+
+n.reverse()
+print(n)
+
+n.append(10)
+print(n)
+n.insert(5,11)
+print(n)
+n.extend(nn)
+print(n)
